81ee6e4752d1d664ae470b9f9756606679b08844
Emmanuel Mutugi <smontana025@gmail.com>
2024-11-16 00:19:10 +0300

    completed

--- src/App.js
+++ src/App.js
@@ -7,14 +7,17 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Wishlist from "./components/Wishlist";
 import Footer from "./components/Footer";
+//wraps the entire application to enable routing
+//specifies what component to render for a given URL path.
+// ensures only one route is matched and rendered at a time
 
 const App = () => {
-  const [cars, setCars] = useState([]);
+  const [cars, setCars] = useState([]); //store list of cars fetched(empy array initially)
   const [wishlist, setWishlist] = useState(() => {
     const savedWishlist = localStorage.getItem("wishlist");
     return savedWishlist ? JSON.parse(savedWishlist) : [];
   });
-
+  //get data from localStorage to wishlist data between sessions.
   useEffect(() => {
     fetch("https://car-dealership-backend-2.onrender.com/cars")
       .then((response) => response.json())
@@ -24,35 +27,42 @@ const App = () => {
 
   const addToWishlist = (car) => {
     setWishlist((prevWishlist) => {
-      const updatedWishlist = [...prevWishlist, car];
+      // useState hook to updat state variable wishlist
+      const updatedWishlist = [...prevWishlist, car]; //list of carz in the wishlist before new car
       localStorage.setItem("wishlist", JSON.stringify(updatedWishlist));
-      return updatedWishlist;
+      return updatedWishlist; //update based on the latest state value and return newState
     });
   };
 
   const removeFromWishlist = (carId) => {
+    //unique identifier for the car to be removed
     setWishlist((prevWishlist) => {
+      //update wishlist state using a callback function.
       const updatedWishlist = prevWishlist.filter((car) => car.id !== carId);
       localStorage.setItem("wishlist", JSON.stringify(updatedWishlist));
       return updatedWishlist;
     });
   };
-
+  //create new array only with car not mtching carId
   return (
     <Router>
-      <Navbar />
+      <Navbar /> {/*/ visible across all routes*/}
       <div className="app">
         <Switch>
+          {" "}
+          {/* only one root renderd at a time */}
           <Route
             exact
             path="/"
             render={() => <Home cars={cars} addToWishlist={addToWishlist} />}
-          />
+          />{" "}
+          {/* Passes props C&ATWST to the <Home> component dynamically*/}
           <Route
             path="/garage"
             render={() => <Garage cars={cars} addToWishlist={addToWishlist} />}
           />
-          <Route path="/about" component={About} />
+          <Route path="/about" component={About} />{" "}
+          {/* component prop since no additional props need to be passed*/}
           <Route path="/contact" component={Contact} />
           <Route
             path="/wishlist"
@@ -62,10 +72,11 @@ const App = () => {
                 removeFromWishlist={removeFromWishlist}
               />
             )}
-          />
+          />{" "}
+          {/*use (render) to pass a function that returns the component*/}
         </Switch>
       </div>
-      <Footer /> {/* add footer component here */}
+      <Footer /> {/* PASSED AT BOTTOM OF EVRY PAGE NOT REGARDING ROUTES*/}
     </Router>
   );
 };
